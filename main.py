1405d00d8061f0d80cdba2a13038d02335c44ef7
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-02-18 19:03:31 +0000

    final test

--- main.py
+++ main.py
@@ -27,42 +27,23 @@ def find_mismatch(text):
 
 
 def main():
-    choice = input("Enter F to choose file or I to choose input: ").strip().upper()
-    text = ""
-    if choice == "F":
-        file_name = input("Enter file name: ")
-        if os.path.isfile(file_name):
-            with open(file_name) as f:
-                text = f.read()
-        else:
-            print("File does not exist.")
-            return
-    elif choice == "I":
-        text = input("Enter brackets: ")
-    else:
-        print("Invalid choice.")
-        return
-
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    print(mismatch)
    
-   # letter = input("Choose (F) to input file path or (I) to input text: ")
-    #if letter.strip().lower() == "f":
-      #  path = input("Enter file path: ")
-       # if os.path.exists(path):
-           # with open(path, "r") as f:
-             #   text = f.read()
-             #   mismatch = find_mismatch(text)
-             #   print(mismatch)
-   #    else:
-#print("Invalid file path!")
-   # elif letter.strip().lower() == "i":
-      #  text = input("Enter text: ")
-      #  mismatch = find_mismatch(text)
-       # print(mismatch)
-  #  else:
-      #  print("Invalid input")
+    letter = input("Choose (F) to input file path or (I) to input text: ")
+    if letter.strip().lower() == "f":
+        path = input("Enter file path: ")
+        if os.path.exists(path):
+            with open(path, "r") as f:
+                text = f.read()
+                mismatch = find_mismatch(text)
+                print(mismatch)
+        else:
+           print("Invalid file path!")
+    elif letter.strip().lower() == "i":
+        text = input("Enter text: ")
+        mismatch = find_mismatch(text)
+        print(mismatch)
+    else:
+        print("Invalid input")
 
 
     #text=input()
