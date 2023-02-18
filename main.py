07812276eed00c87aba1e2df9fe8375e3dbbc284
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-02-18 14:00:16 +0000

    test

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
             if opening_brackets_stack:
                 return opening_brackets_stack[0].position
-            return" Success"
+            else :
+             return" Success"
                 
             #pass
 
